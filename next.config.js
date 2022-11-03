0a54a9ce479f8c69ed91885b21405823f9370f6b
javiergb85 <javiergb85@gmail.com>
2022-11-03 14:55:06 -0400

    changing config nexjs

--- next.config.js
+++ next.config.js
@@ -2,9 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental:{
+    outputStandalone:true
+  },
   images:{
     domains:['raw.githubusercontent.com']
   }
 }
 
 module.exports = nextConfig
+
+ 
